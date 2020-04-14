a4e40637ff90a503fde4ffa13049ef954b012c09
Will Thompson <williamjamesthompson@gmail.com>
2020-04-14 16:42:19 -0500

    Added node-fetch

--- components/TokenForm.js
+++ components/TokenForm.js
@@ -3,7 +3,7 @@ import { Query } from "react-apollo";
 import gql from "graphql-tag";
 import { TextField, Button, Toast, Frame } from "@shopify/polaris";
 import { LoadingText } from "../components/Loading";
-const fetch = require('node-fetch');
+import fetch from 'node-fetch';
 
 
 function TokenForm(props) {
